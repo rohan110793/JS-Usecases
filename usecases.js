fa7ffb9999e78a745841b8345f30d5db9c888e07
Rohan1107 <rohankgaikwad@gmail.com>
2025-04-23 18:50:56 -0700

    new concepts

--- usecases.js
+++ usecases.js
@@ -1,3 +1,5 @@
+// Javascript usecases
+
 // 1. access div element by id
 
 // 1.1 access div element with id: div1 and then change/replace the text in that div
@@ -17,6 +19,49 @@ const element2 = document.getElementById("div2");
 
 //use element2's inner HTML to add/append new text to the text in that element
 element2.innerHTML += "div2 is accessed and new text is appended to the element";
+
+// 1.3 check difference between InnerHTML, innerText and textContext on console
+const element3 = document.getElementById("div3");
+
+// print innerHTML content
+console.log(element3.innerHTML);
+// reads and returns both the HTML markup and the text content of the elements
+// displays text from the element
+
+// print innerText content
+console.log(element3.innerText);
+// reads and returns only text as it appears on the screen in the element
+// It ignores HTML tags
+// does not include text that is hidden with CSS styles
+// When you need to account for styles, you should consider using innerText.
+
+// print textContent content
+console.log(element3.textContent);
+// reads and returns text only as it appears in the html element regardless of whether
+// it is rendered on the screen or not
+// It ignores HTML tags
+// In situations where performance is a concern and
+// you don't need to account for styles,
+// textContent might be a more efficient choice compared to innerText
+
+
+// Testing and debugging tools/functions
+// console.log()
+// window.alert() or alert()
+// document.write()
+
+//console.log("abc");
+
+//window.alert("hey this an alert!");
+//alert("this is also an alert!!");
+
+//document.write(4+6);
+//print output on document after all the html tags
+//will print in the end no matter where it is mentioned in .js file
+//only used for testing purposes
+
+// to print the content of the current window with printer
+// window.print();
 
 
 
@@ -38,3 +83,4 @@ parentElement7.appendChild(newElement7); // append div70 as child element to par
 
 // Access new created div element by id and set some text into it
 document.getElementById("div70").innerHTML = "New 'div' element created and text is added into it";
+
