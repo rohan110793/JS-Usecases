b2069e85dff8291230de5fb307787c5cd8a5a96c
Rohan1107 <rohankgaikwad@gmail.com>
2025-05-07 23:25:02 -0700

    constructors

--- usecases.js
+++ usecases.js
@@ -342,6 +342,14 @@ function editTask(index) {
     displayTasks();
   }
 
+  if (updatedTaskText !== null) {
+    taskList[index] = updatedTaskText;
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    displayTasks();
+    if (updatedTaskText !== null) {
+    taskList[index] = updatedTaskText;
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    displayTasks();
 }
 
 */
